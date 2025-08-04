115d4899287025a5d011ec6dd2c4dfa23a46c060
Felix Kim <felixkim@Mac.lan>
2025-08-03 17:34:51 -0700

    Added somethings

--- Onew.py
+++ Onew.py
@@ -1,2 +1,14 @@
 print("This is Onew.py")
 
+number = 3
+
+guess = int(input("What is the number"))
+
+if guess < 3:
+    print("too small")
+
+elif guess > 3:
+    print("Too big")
+
+else:
+    print("Bingo")
